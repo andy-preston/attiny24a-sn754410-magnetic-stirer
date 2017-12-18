893bf7b3e74a9c73a0875b60543c68679880d93d
Andy Preston <edgeeffect@gmail.com>
2017-12-18 21:02:24 +0000

    make it so that assemble will work for atTiny24A

--- main.asm
+++ main.asm
@@ -1,6 +1,8 @@
 ; ATtiny24A - L293D laboratory stirer
 
-            .include "TN13DEF.INC"
+            .nolist
+            .include "/usr/local/share/avra/tn24Adef.inc"
+            .list
 
             .def rDelay=R16
             .def rCount=R17
@@ -12,20 +14,18 @@
 
             .org 0x0000             ; reset vector
 
-            ldi rTemp, low(RAMEND)  ; stack setup; set SPH:SPL to RAMEND
+            ldi rTemp, RAMEND       ; stack setup; set SPL to RAMEND
             out SPL, rTemp
-            ldi rTemp, high(RAMEND)
-            out SPH, rTemp
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-            ldi rTemp, 0b0000_1111
+            ldi rTemp, 0b00001111
             out DDRA, rTemp         ; set lower 4 PortA pins as output
             out DDRB, rTemp         ; set lower 4 PortB pins as output
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-            ldi rOut, 0b0001_0001   ; output bits
+            ldi rOut, 0b00010001    ; output bits
             mov rMask, rOut         ; initial mask is the same
 mainloop:   out PORTA, rOut         ; ouput lower 4 bits to Port A
             out PORTB, rOut         ; and Port B
@@ -49,24 +49,24 @@ mainloop:   out PORTA, rOut         ; ouput lower 4 bits to Port A
 pause:      ldi rTemp, 2            ; select ADC channel 4 (PA4)
             out ADMUX, rTemp
 
-            ldi rTemp, 0b1110_0111  ; from left to right:
+            ldi rTemp, 0b11100111   ; from left to right:
                                     ; ADC Enable,
                                     ; Start Conversion,
                                     ; Free-Running Mode,
                                     ; write zero to ADC Int flag,
                                     ; disable int,
                                     ; prescaler: 111 for XTAL/128
-            out ADCSR, rTemp
+            out ADCSRA, rTemp
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
             mov rCount, rDelay      ; start the proper pause routine
 
 pauseloop:  in rTemp, TIFR0         ; read timer status
-            andi rTemp, 0b0000_0010 ; check overflow flag
+            andi rTemp, 0b00000010  ; check overflow flag
             breq pauseloop
 
-            ldi rTemp, 0b0000_0010  ; reset overflow (with a 1 not a zero)
+            ldi rTemp, 0b00000010   ; reset overflow (with a 1 not a zero)
             out TIFR0, rTemp
 
             dec rCount
@@ -74,8 +74,8 @@ pauseloop:  in rTemp, TIFR0         ; read timer status
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-adcloop:    in rTemp, ADCSR
-            andi rTemp, 0b0100_0000 ; has analogue conversion finsihed
+adcloop:    in rTemp, ADCSRA
+            andi rTemp, 0b01000000  ; has analogue conversion finsihed
             breq adcloop
 
             in rDelay, ADCL         ; get the last ADC result, low byte first,
