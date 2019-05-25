bd471ad45dcad1406ebc3a817260401ca716304f
Andy Preston <edgeeffect@gmail.com>
2019-05-25 16:26:07 +0100

    fix registers.asm

--- lib/registers.asm
+++ lib/registers.asm
@@ -1,13 +1,9 @@
 ; r1, r2 and r3 are used by test/test-delay.asm
 
-.def ioReg = r7
 .def calcReg = r8
 .def saveReg = r10
 .def saveLReg = r10
 .def saveHReg = r11
-.def inputReg = r12
-.def inputLReg = r12 ; inputReg is sometimes 16 bit
-.def inputHReg = r13
 .def dummyJunkReg = r14
 .def dummyZeroReg = r15
 
@@ -15,9 +11,11 @@
 .def quickReg = r16     ; very short intermediate values
 .def countReg = r17
 .def shiftReg = r18
-.def lhReg = r24
-.def lowReg = r24
-.def highReg = r25
+.def inputReg = r22
+.def inputLReg = r22 ; inputReg is sometimes 16 bit
+.def inputHReg = r23
+.def adccReg = r24
+.def ioReg = r25
 ; XL, XH, YL, YH, ZL, ZH don't seem to get defined in my version of GAVRASM
 .def X = r26
 .def XL = r26
