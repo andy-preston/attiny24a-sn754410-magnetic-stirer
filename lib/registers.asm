e0fb450ae0400488e6287ced0bbd60cefd143f5a
Andy Preston <edgeeffect@gmail.com>
2019-05-27 21:21:51 +0100

    fix registers

--- lib/registers.asm
+++ lib/registers.asm
@@ -1,28 +1,12 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
-; TODO: This is the registers file from Crstal-Palace - make sure if they're
-; not used they're not here anymore
-;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ; r1, r2 and r3 are used by test/test-delay.asm
-
-.def calcReg = r8
-.def saveReg = r10
-.def saveLReg = r10
-.def saveHReg = r11
-.def dummyJunkReg = r14
-.def dummyZeroReg = r15
 
 ; only registers r16-r31 can use immediate
 .def quickReg = r16     ; very short intermediate values
 .def countReg = r17
 .def shiftReg = r18
-.def inputReg = r22
-.def inputLReg = r22 ; inputReg is sometimes 16 bit
-.def inputHReg = r23
-.def adcReg = r24
-.def ioReg = r25
+.def inputLReg = r19
+.def inputHReg = r20
+.def ioReg = r21
 ; XL, XH, YL, YH, ZL, ZH don't seem to get defined in my version of GAVRASM
 .def X = r26
 .def XL = r26
@@ -36,9 +20,6 @@
 
 .macro setupStackAndReg
     CLI
-
     LDI quickReg, RAMEND
     OUT SPL, quickReg
-
-    CLR dummyZeroReg
 .endm
